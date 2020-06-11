faed7be22070bd0f63bbcf5b18f0ef634cebc3ba
jasbirsinghrana <jasbirs@mindfiresolutions.com>
2020-06-11 20:29:39 +0530

    Add nearby users

--- src/components/Posts/Post/TravellersNearby.js
+++ src/components/Posts/Post/TravellersNearby.js
@@ -5,6 +5,7 @@ import React from 'react';
 import Box from '@material-ui/core/Box';
 import Paper from '@material-ui/core/Paper';
 import Button from '@material-ui/core/Button';
+import Avatar from '@material-ui/core/Avatar';
 import Divider from '@material-ui/core/Divider';
 import CardMedia from '@material-ui/core/CardMedia';
 import Typography from '@material-ui/core/Typography';
@@ -34,6 +35,14 @@ const useStyles = makeStyles((theme: Theme) => ({
     marginRight: 0,
     marginLeft: 'auto',
     background: '#fff'
+  },
+  users: {
+    display: 'flex'
+  },
+  user: {
+    margin: theme.spacing(2.5),
+    width: 80,
+    height: 80
   }
 }));
 
@@ -43,12 +52,19 @@ const TravellersNearby = () => {
   	
   	return (
           <Paper variant="outlined" className={classes.TravellersNearbyRoot}>
-            <Box component="div" p={1} className={classes.head}>
+            <Box p={1} className={classes.head}>
               <Typography variant="h6" className={classes.title}>
                 Travellers Nearby
               </Typography>
-              <Button variant="contained" className={classes.viewAllButton}>View All</Button>
+              <Button variant="contained" className={classes.viewAllButton}>View All</Button> 
             </Box>
+            <Box className={classes.users}>
+              <Avatar className={classes.user} src="https://static.intercomassets.com/avatars/3332512/square_128/Screenshot_2020-04-11_at_10.51.00-1586591829.png?1586591829" />
+              <Avatar className={classes.user} src="https://static.intercomassets.com/avatars/3332512/square_128/Screenshot_2020-04-11_at_10.51.00-1586591829.png?1586591829" />
+              <Avatar className={classes.user} src="https://static.intercomassets.com/avatars/3332512/square_128/Screenshot_2020-04-11_at_10.51.00-1586591829.png?1586591829" />
+              <Avatar className={classes.user} src="https://static.intercomassets.com/avatars/3332512/square_128/Screenshot_2020-04-11_at_10.51.00-1586591829.png?1586591829" />
+              <Avatar className={classes.user} src="https://static.intercomassets.com/avatars/3332512/square_128/Screenshot_2020-04-11_at_10.51.00-1586591829.png?1586591829" />
+            </Box> 
           </Paper>        
   	);
 }
