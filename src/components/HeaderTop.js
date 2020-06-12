bdde55a0d498097dc36edda3c796cfe284a6f909
jasbirsinghrana <jasbirs@mindfiresolutions.com>
2020-06-12 17:46:09 +0530

    fixed sidebars responsiveness

--- src/components/HeaderTop.js
+++ src/components/HeaderTop.js
@@ -5,6 +5,7 @@ import React from 'react';
 
 //material UI
 import Box from '@material-ui/core/Box';
+import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
 
 // styles
@@ -13,7 +14,8 @@ import { makeStyles, Theme } from '@material-ui/core/styles';
 const useStyles = makeStyles((theme: Theme) => ({
   email: {
     color: '#a4a4bc',
-    fontSize: theme.spacing(1.7)
+    fontSize: theme.spacing(1.7),
+    paddingLeft: theme.spacing(5),
   },
   phone: {
     color: '#a4a4bc',
@@ -30,10 +32,11 @@ const useStyles = makeStyles((theme: Theme) => ({
     marginRight: theme.spacing(2)
   },
   signUpBox: {
-    float: 'right',
-    [theme.breakpoints.down('sm')]: {
-      float: 'none',
-    },
+    paddingRight: theme.spacing(5),
+  },
+  gridRight: {
+    direction: 'rtl',
+    paddingRight: theme.spacing(5),
   }
 }));
 
@@ -42,27 +45,31 @@ const Layout = () => {
   const classes = useStyles();
     
     return (
-          <Box component="div" p={1} bgcolor="#3d3b50">
-            <Box component="span" ml={5}>
+      <Box component="div" p={1} bgcolor="#3d3b50">
+        <Grid container spacing={3}>
+            <Grid item xs={12} lg={6} md={6}>
               <Typography component="span" className={classes.email}>
-                info@attractivemyamar.com
+              info@attractivemyamar.com
               </Typography>
-            </Box>
-            <Box component="span" mr={5} className={classes.signUpBox}>
-              <Typography component="span" className={classes.loginSignupText}>
-                Login
-              </Typography>
-              <Typography component="span" className={classes.loginSignupPipe}>
-                | 
-              </Typography>
-              <Typography component="span" className={classes.loginSignupText}>
-                Signup
-              </Typography>
-              <Typography component="span" className={classes.phone}>
-                Call Us: +91 123-456-7890
-              </Typography>
-            </Box>
-          </Box>
+            </Grid>
+            <Grid item xs={12} lg={6} md={6} className={classes.gridRight}>
+              <Box className={classes.signUpBox}>
+                <Typography component="span" className={classes.loginSignupText}>
+                  Login
+                </Typography>
+                <Typography component="span" className={classes.loginSignupPipe}>
+                  | 
+                </Typography>
+                <Typography component="span" className={classes.loginSignupText}>
+                  Signup
+                </Typography>
+                <Typography component="span" className={classes.phone}>
+                  Call Us: +91 123-456-7890
+                </Typography>
+              </Box>
+            </Grid>
+        </Grid>
+      </Box>
     );
 }
 
